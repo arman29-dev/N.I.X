1ead7cf7ae434f1ce9957411307b6b4285ec0276
Arman Das <armandevilk282004@gmail.com>
2025-09-19 00:28:17 +0530

    🔧 user fetch issue

--- app/web/pages.py
+++ app/web/pages.py
@@ -299,12 +299,13 @@ async def logout(req: Request, session: SessionDep, current_user_uid: str|None=N
 @login_required()
 async def dashboard(req: Request, uid: str, session: SessionDep, current_user_uid: str|None=None):
     devices = get_all_devices(owner_uid=uid, session=session)
+    user = get_user_by_id(uid, session)
 
     return templates.TemplateResponse(
         "dashboard.html",
         {
             "request": req,
             "devices": devices,
-            "user": current_user_uid,
+            "user": user,
         }
     )
